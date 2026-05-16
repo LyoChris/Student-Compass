e299c9485cec8f5d295905df97da0e119a9be3e3
LyoChris <cosminpatranea@gmail.com>
2026-05-16 11:21:20 +0300

    adaugat backend

---
+++ backend-compas/src/main/java/org/backendcompas/BackendCompasApplication.java
@@ -0,0 +1,13 @@
+package org.backendcompas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCompasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCompasApplication.class, args);
+    }
+
+}
---
+++ backend-compas/src/test/java/org/backendcompas/BackendCompasApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backendcompas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCompasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
